a7ace954bf473970643cc9cd768b7a927406586c
onesine <onesine@dmdconsul.com>
2022-10-20 19:24:36 +0100

    Rename title

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ const App = () => {
                                     <AccordionHeader>
                                         {({open}) => (
                                             <>
-                                                Title 1
+                                                Title 3.1
                                                 <svg className={`w-6 h-6 transform transition duration-300 ${!open ? '' : ' rotate-90'}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
                                             </>
                                         )}
@@ -84,7 +84,7 @@ const App = () => {
                                     <AccordionHeader>
                                         {({open}) => (
                                             <>
-                                                Title 2
+                                                Title 3.2
                                                 <svg className={`w-6 h-6 transform transition duration-300 ${!open ? '' : ' rotate-90'}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
                                             </>
                                         )}
@@ -96,7 +96,7 @@ const App = () => {
                                                 <AccordionHeader>
                                                     {({open}) => (
                                                         <>
-                                                            Title 1
+                                                            Title 3.2.1
                                                             <svg className={`w-6 h-6 transform transition duration-300 ${!open ? '' : ' rotate-90'}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
                                                         </>
                                                     )}
@@ -116,7 +116,7 @@ const App = () => {
                                                 <AccordionHeader>
                                                     {({open}) => (
                                                         <>
-                                                            Title 2
+                                                            Title 3.2.2
                                                             <svg className={`w-6 h-6 transform transition duration-300 ${!open ? '' : ' rotate-90'}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
                                                         </>
                                                     )}
